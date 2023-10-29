a9ccdb40856cbf680cbde417589b7bc0a183f56a
Ashish Lal <ashishlal2003@gmail.com>
2023-10-29 16:04:40 +0530

    backend complete

--- server/controllers/transcript.js
+++ server/controllers/transcript.js
@@ -1,22 +1,30 @@
-//For YouTube Specific Transcriptions
+const { TextServiceClient } = require("@google-ai/generativelanguage").v1beta2;
 
-const {YoutubeTranscript} = require('youtube-transcript');
+const { GoogleAuth } = require("google-auth-library");
+const transcript = require("../utilities/transcript");
+
+
+const MODEL_NAME = "models/text-bison-001";
+const API_KEY = "AIzaSyCSLD_VAewOZNQytOh4hV_FmO8sr0rPR0A";
+
+const client = new TextServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
 
 const getTranscript = async (req, res) => {
     try {
-        const {videoURL} = req.body;
-        const transcript = await YoutubeTranscript.fetchTranscript(videoURL).then((transcript) => {
-            const completeText = transcript.map((entry) => entry.text).join(' ');
-            return completeText;
+        const { videoURL } = req.body;
+        const toTranscript = await transcript(videoURL);
+        const result = await client.generateText({
+            model: MODEL_NAME,
+            prompt: {
+              text: "Give me the following with correct punctuations and split it into multiple paragraphs." + toTranscript,
+            },
           });
-      
-          // Respond with the complete text instead of the transcript
-          res.status(200).json({ transcript: transcript });
+          res.status(200).json(result[0].candidates[0].output);
     } catch (error) {
-        res.status(500).json({message: error.message});
+      res.status(500).json({ message: error.message });
     }
-}
-
-module.exports = {
-    getTranscript
-}
+  };
+  
+module.exports = { getTranscript };
--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const transcriptRoutes = require('./routes/transcript');
+const generativeRoutes = require('./routes/generative');
+require('dotenv').config();
+
 
 const app = express();
 
 app.use(morgan('combined'));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/transcript', transcriptRoutes);
+app.use('/generative', generativeRoutes);
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 })
