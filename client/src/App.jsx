5df21673b80123dfd8c070b19083ec7e0eedd710
Nancy <ynancy030@gmail.com>
2023-11-04 14:13:51 +0530

    new design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css"
 import Home from "./Pages/Home/Home"
 import Main from "./Pages/Main/Main"
+import Main2 from "./Pages/Main/Main2"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Main />} />
+        <Route path="/chat" element={<Main2 />} />
       </Routes>
     </Router>
   )
