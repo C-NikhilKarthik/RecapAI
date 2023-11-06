8e29b4fe96c7f92f04ae5d5973534ced55b56c44
Nancy <ynancy030@gmail.com>
2023-11-06 12:22:51 +0530

    modal added

---
+++ client/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AboutDiv() {
+  return (
+    <div className='absolute top-[10%] right-[32%] h-[600px] w-[500px] bg-[#2F3136] rounded-md p-3 text-white shadow-sm shadow-gray-700'>
+      <h2 className='font-bold text-xl text-center underline mt-4'>How to Use</h2> 
+      <ul className='mt-4'>
+        <li>hello world</li>
+        <li>Hehe</li>
+      </ul>
+    </div>
+  )
+}
